483015607fc67390481663c02905906a302cc528
kduckets <kmditroia@gmail.com>
2017-08-27 13:02:22 -0700

    bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: ['./app/main.js'],
     output: {
-        path: './build',
+        path: './public',
         filename: 'bundle.js',
         publicPath: 'http://localhost:8080/assets'
     },
